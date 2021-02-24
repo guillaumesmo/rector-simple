e818e306862479604723cd8de3e6b607213fb1df
Guillaume Smolders <guillaumesmo@gmail.com>
2021-02-24 22:00:12 +0100

    Version 0.9.31

--- sets/FuncCallToConstFetchRector.php
+++ sets/FuncCallToConstFetchRector.php
@@ -3,5 +3,5 @@
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator) {
-    $containerConfigurator->services()->set(\Rector\CodingStyle\Rector\FuncCall\FunctionCallToConstantRector::class);
+    $containerConfigurator->services()->set(\Rector\Transform\Rector\FuncCall\FuncCallToConstFetchRector::class);
 };
