b10a3d86e2d456e88c1709b0fb0169bb9191cd62
Guillaume Smolders <guillaumesmo@gmail.com>
2021-04-25 14:02:31 +0200

    Version 0.10.7

--- sets/RemoveUnusedPrivateMethodParameterRector.php
+++ sets/RemoveUnusedPrivateMethodParameterRector.php
@@ -3,5 +3,5 @@
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator) {
-    $containerConfigurator->services()->set(\Rector\DeadCode\Rector\ClassConst\RemoveUnusedPrivateConstantRector::class);
+    $containerConfigurator->services()->set(\Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPrivateMethodParameterRector::class);
 };
