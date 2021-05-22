3746c381f2129748fa83d4f53bb8cc07245474a2
Guillaume Smolders <guillaumesmo@gmail.com>
2021-05-23 01:47:33 +0200

    Version 0.11.0

--- sets/DowngradeScalarTypeDeclarationRector.php
+++ sets/DowngradeScalarTypeDeclarationRector.php
@@ -3,5 +3,5 @@
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator) {
-    $containerConfigurator->services()->set(\Rector\DowngradePhp70\Rector\FunctionLike\DowngradeTypeDeclarationRector::class);
+    $containerConfigurator->services()->set(\Rector\DowngradePhp70\Rector\FunctionLike\DowngradeScalarTypeDeclarationRector::class);
 };
